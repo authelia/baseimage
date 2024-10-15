6b04ed09e8cdac6e7082184c1f0e93c8827c78f7
Amir Zarrinkafsh <nightah@me.com>
2024-10-15 14:16:44 +1100

    feat: add tag and buildkite trigger logic

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
-set -u
 
+BUILDTAGS=""
+REGISTRIES="docker.io ghcr.io"
 REPOSITORY="authelia/baseimage"
-TAG="latest"
+TAGS=""
+
+if [[ "${BUILDKITE_BRANCH}" =~ ^renovate/ ]]; then
+  TAGS="renovate"
+elif [[ "${BUILDKITE_BRANCH}" != "master" ]] && [[ ! "${BUILDKITE_BRANCH}" =~ .*:.* ]]; then
+  TAGS="${BUILDKITE_BRANCH}"
+elif [[ "${BUILDKITE_BRANCH}" != "master" ]] && [[ "${BUILDKITE_BRANCH}" =~ .*:.* ]]; then
+  TAGS="PR${BUILDKITE_PULL_REQUEST}"
+elif [[ "${BUILDKITE_BRANCH}" == "master" ]] && [[ "${BUILDKITE_PULL_REQUEST}" == "false" ]]; then
+  TAGS="latest"
+fi
+
+[[ ${BUILDKITE_BUILD_NUMBER} != "" ]] && TAGS+=" BK${BUILDKITE_BUILD_NUMBER}"
+[[ ${AUTHELIA_RELEASE} != "" ]] && TAGS+=" ${AUTHELIA_RELEASE}"
+
+for REGISTRY in ${REGISTRIES}; do for TAG in ${TAGS}; do BUILDTAGS+="--tag ${REGISTRY}/${REPOSITORY//image}:${TAG} "; done; done
 
 cat << EOF
 steps:
   - label: ":docker: Build and Deploy"
-    command: "docker build --tag ${REPOSITORY//image}:${TAG} --platform linux/amd64,linux/arm/v7,linux/arm64 --builder buildx --pull --push ."
+    command: "docker build ${BUILDTAGS::-1} --label org.opencontainers.image.source=https://github.com/${REPOSITORY} --platform linux/amd64,linux/arm/v7,linux/arm64 --builder buildx --pull --push ."
     concurrency: 1
     concurrency_group: "baseimage-deployments"
     agents:
